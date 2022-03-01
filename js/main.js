4ceacbc6d4c6baf5a07e87afbc30f674874379a5
mdaias <mdaias29@gmail.com>
2022-03-01 11:35:55 +0600

    add phone details part

--- js/main.js
+++ js/main.js
@@ -13,11 +13,12 @@ const searchPhone = () => {
 const displayResult = phones => {
     
     const divContainer = document.getElementById('phone-result');
+    
     //clear previous result
     divContainer.textContent = '';
     
     phones.forEach(phone =>{
-        // console.log(phone.phone_name)
+        // console.log(phone.slug)
 
         const div = document.createElement('div');
         div.classList.add('col');
@@ -28,11 +29,30 @@ const displayResult = phones => {
                     <h4 class="card-title">${phone.phone_name}</h4>
                     <h5 class="card-title">Brand: <span>${phone.brand}</span></h5>
                 </div>
+
                 <div class="d-grid gap-2 col-6 mx-auto">
-                    <button class="btn btn-primary" type="button">Details</button>
+                    <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary" type="button">Details</button>
                 </div>
             </div>
         `
         divContainer.appendChild(div);
     })
+}
+
+//Phone details data load
+const phoneDetails = id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaydetails(data) )
+}
+
+//display phone details
+const displaydetails = details => {
+    console.log(details);
+    const {brand,image,name} = details.data;
+    const {chipSet,displaySize,memory,storage} =details.data.mainFeatures;
+    const {Bluetooth,GPS,NFC,Radio,USB,WLAN} = details.data.others;
+
+    console.log(Bluetooth,chipSet)
 }
